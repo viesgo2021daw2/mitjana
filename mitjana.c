81e63ec8299b769b2afc2fd9e4b9e25f914a0419
viesgo2021daw2 <viesgo2021daw2@protonmail.com>
2020-11-28 23:32:21 +0100

    Segon commit del projecte mitjana

--- mitjana.c
+++ mitjana.c
@@ -13,7 +13,7 @@ int main() {
 		suma=suma+nou_valor;
 		numero_repeticions=numero_repeticions+1;
 		printf("Vols continuar (y/n): ");
-		scanf(" %c",&consulta); 
+		scanf(" %s",&consulta); 
 	}
 	mitja=suma/numero_repeticions;
 	printf("La mitjana resultant és: %.2f\n",mitja);	
